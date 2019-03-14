5672d2dd91efb015b777ff3630697a4783d30398
student <student@NPTU25.local>
2019-03-14 10:11:41 +0800

    Dice Game
    - Add Shake Gesture

---
+++ Course_Morning/20190314.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var str = "Hello, playground"
+
+func add (){
+    let a = 5
+    let b = 9
+    print(a + b)
+}
+add()
+
+
+func calc(x:Int, y:Int) -> Int{
+    return x + y
+}
+print(calc(x: 2, y: 2))
--- Course_Morning/Dice/Dice/ViewController.swift
+++ Course_Morning/Dice/Dice/ViewController.swift
@@ -23,20 +23,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        diceIndex1 = Int(arc4random_uniform(UInt32(6)))
+//        leftDice.image = UIImage(named: diceImageArray[diceIndex1])
+//        diceIndex2 = Int(arc4random_uniform(UInt32(6)))
+//        rightDice.image = UIImage(named: diceImageArray[diceIndex2])
+        randomDice()
     }
 
     @IBAction func button1(_ sender: Any) {
-        
+        randomDice()
         /* Left side dice */
-        diceIndex1 = Int(arc4random_uniform(UInt32(6)))
-        leftDice.image = UIImage(named: diceImageArray[diceIndex1])
-        print("Dice1:", diceIndex1)
+//        diceIndex1 = Int(arc4random_uniform(UInt32(6)))
+//        leftDice.image = UIImage(named: diceImageArray[diceIndex1])
+//        print("Dice1:", diceIndex1)
         
         
         /* Right side dice */
+//        diceIndex2 = Int(arc4random_uniform(UInt32(6)))
+//        rightDice.image = UIImage(named: diceImageArray[diceIndex2])
+//        print("Dice2:", diceIndex2)
+    }
+    
+    func randomDice() {
+        diceIndex1 = Int(arc4random_uniform(UInt32(6)))
+        leftDice.image = UIImage(named: diceImageArray[diceIndex1])
         diceIndex2 = Int(arc4random_uniform(UInt32(6)))
         rightDice.image = UIImage(named: diceImageArray[diceIndex2])
-        print("Dice2:", diceIndex2)
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == UIEvent.EventSubtype.motionShake {
+            randomDice()
+        }
     }
     
 }
