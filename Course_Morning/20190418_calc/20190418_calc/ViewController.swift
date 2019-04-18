45913b66a097a3ca1e53068da28dba77d61c253f
student <student@NPTU25.local>
2019-04-18 11:56:00 +0800

    Calculator Layout
    - Auto Layout Practice

---
+++ Course_Morning/20190418_calc/20190418_calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  20190418_calc
+//
+//  Created by student on 2019/4/18.
+//  Copyright © 2019年 trevorwu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
