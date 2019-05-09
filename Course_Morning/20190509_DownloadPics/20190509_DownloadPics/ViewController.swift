93b1dc5ad6de751a38543f82f5630fec6abc1446
MrNegativeTW <wowpapa3232@gmail.com>
2019-05-09 12:20:45 +0800

    隨機壁紙

--- Course_Morning/20190509_DownloadPics/20190509_DownloadPics/ViewController.swift
+++ Course_Morning/20190509_DownloadPics/20190509_DownloadPics/ViewController.swift
@@ -2,28 +2,29 @@
 //  ViewController.swift
 //  20190509_DownloadPics
 //
-//  Created by student on 2019/5/9.
-//  Copyright © 2019年 TxWStudio. All rights reserved.
+//  Created by Trevor Wu on 2019/5/9.
+//  Copyright © 2019年 TxWStudio. All rights reserved/.
 //
+// Device: Any
+// Swift Version: 4
 
 import UIKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var unsplashImage: UIImageView!
-    @IBOutlet weak var download: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-//        getUnsplashImage()
-        getUnsplashImage2()
+        getUnsplashImage()
+//        getUnsplashImage2()
     }
     
     func getUnsplashImage() {
         do {
-            let imageSource = URL(string: "https://source.unsplash.com/random")
+            let imageSource = URL(string: "https://source.unsplash.com/random/2048x1536")
             let imageData = try Data(contentsOf: imageSource!)
             self.unsplashImage.image = UIImage(data: imageData)
         } catch {
@@ -31,7 +32,20 @@ class ViewController: UIViewController {
         }
     }
     
-//    func getUnsplashImage2() {
+    @IBAction func savePhoto(_ sender: Any) {
+        let imageData = UIImagePNGRepresentation(unsplashImage.image!)
+        let compresedImage = UIImage(data: imageData!)
+        UIImageWriteToSavedPhotosAlbum(compresedImage!, nil, nil, nil)
+        
+    }
+    
+    
+    @IBAction func update(_ sender: Any) {
+        getUnsplashImage()
+    }
+    
+    // THIS DOESN'T WORK
+    //    func getUnsplashImage2() {
 //        let imageSource = URL(string: "https://source.unsplash.com/random")
 //        let imageData = URLRequest(url: imageSource!)
 //        let task = URLSession.shared.dataTask(with: imageData) { (data, response, error) in
